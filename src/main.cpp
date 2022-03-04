34ff104b71a2b210e9722c0ab95c9a20bfca2373
Matheus Tavares <matheustavarestrindade@hotmail.com>
2022-03-04 12:52:55 -0300

    added dependenci

--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,24 @@
 #include <TracerRegisters.h>  // empty library for potential future use
-#include <WebServer.h>
-#include <WiFi.h>
 
-const char* ssid = "Rede trindade";
-const char* password = "cpmn2018";
+#include "HomeAutomation.h"
 
 #define SERIAL_COM Serial2  // RX, TX
 #define MAX485_DE 13
 #define MAX485_RE_NEG 13
-#define NETWORK_PORT 7858
 #define RELAY_GPIO 12
 
 #define TRACER_INSTANCE TracerRegisters(MAX485_RE_NEG, MAX485_DE, SERIAL_COM);
 
+char* device_name = "Epever Solar Monitor";
 TracerRegisters* tracerRegisters;
-WebServer server(NETWORK_PORT);
-std::string convertToString(char* a, int size) {
-    int i;
-    std::string s = "";
-    for (i = 0; i < size; i++) {
-        s = s + a[i];
-    }
-    return s;
-}
+HomeAutomation automation(device_name);
 
 void handleRequest() {
     char* data;
     tracerRegisters->getRegistersValueInJson(data);
-    server.setContentLength(CONTENT_LENGTH_UNKNOWN);
-    server.sendHeader("Content-Length", (String)(sizeof(data) * sizeof(char)));
-    server.send(200, "text/json", data);
+    automation.setContentLength(CONTENT_LENGTH_UNKNOWN);
+    automation.sendHeader("Content-Length", (String)(sizeof(data) * sizeof(char)));
+    automation.send(200, "text/json", data);
 }
 
 bool isPoweringPC = false;
@@ -39,11 +28,7 @@ void handlePowerOn() {
     digitalWrite(RELAY_GPIO, 1);
     delay(2000);
     digitalWrite(RELAY_GPIO, 0);
-    server.send(200, "text/json", "{\"success\": true}");
-}
-
-void handleDeveceInformation() {
-    server.send(200, "text/json", "{\"success\": true}");
+    automation.send(200, "text/json", "{\"success\": true}");
 }
 
 void setup() {
@@ -53,37 +38,19 @@ void setup() {
 
     pinMode(RELAY_GPIO, OUTPUT);
 
-    WiFi.begin(ssid, password);
-    while (WiFi.status() != WL_CONNECTED) {
-        delay(500);
-        Serial.print(".");
-    }
-    // Print local IP address and start web server
-    Serial.println("");
-    Serial.println("WiFi connected.");
-    Serial.println("IP address: ");
-    Serial.println(WiFi.localIP());
-    server.begin();
-
     tracerRegisters = new TRACER_INSTANCE;
 
-    server.on("/", handleRequest);
-    server.on("/power_on", handlePowerOn);
-
-    server.begin();
+    automation.on("/", handleRequest);
+    automation.on("/power_on", handlePowerOn);
+    automation.setup();
 }
 
 uint64_t lastUpdateTime = millis();
 
 void loop() {
-    if (WiFi.status() != WL_CONNECTED) {
-        ESP.restart();
-        return;
-    }
-
+    automation.networkLoop();
     if (lastUpdateTime < millis()) {
         tracerRegisters->updateModbusInformation();
         lastUpdateTime = millis() + 2000;
     }
-    server.handleClient();
 }
